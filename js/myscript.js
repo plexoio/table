d7c1ad11fce8cbcfd00cd95723c5f0a83db8689f
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-06-17 13:18:11 +0000

    work on jQuery styling and DOM manipulation

---
+++ js/myscript.js
@@ -0,0 +1,14 @@
+$(document).ready(function () { // run only after the dom is fully loaded
+    $('h2').addClass('underline');
+    $('ul').addClass('border');
+    $('').removeClass('');
+    $('').css('');
+    $('').html();
+    $('').html(''); // replace html content
+    $('').remove(); // element
+    $('').empty(); // element
+    $('').text(''); // replace text in element
+    $('').append(); // add to to the end of existing element$('')
+    $('div > p').html();
+    $('div p').html();
+});
